944c9e74feac20e844cee27952b8bc57232a9dd8
jolleyily <muhthoriq73@gmail.com>
2025-11-26 14:50:42 +0700

    feat: add authenticated layout with bottom navigation and drawer

--- app/layouts/authenticated.vue
+++ app/layouts/authenticated.vue
@@ -4,25 +4,34 @@ const route = useRoute()
 const navItems = [
   {
     label: 'Home',
-    icon: 'i-lucide:home',
+    icon: 'i-solar:home-2-linear',
     to: '/',
   },
   {
     label: 'Transactions',
-    icon: 'i-lucide:receipt',
+    icon: 'i-solar:dollar-linear',
     to: '/transactions',
   },
   {
+    icon: 'i-solar:add-circle-linear',
+    label: 'Add',
+    onClick: () => {
+      open.value = true
+    }
+  },
+  {
     label: 'Analytics',
-    icon: 'i-lucide:chart-line',
+    icon: 'i-solar:graph-linear',
     to: '/analytics',
   },
   {
     label: 'Profile',
-    icon: 'i-lucide:user',
+    icon: 'i-solar:user-linear',
     to: '/profile',
   },
 ]
+
+const open = ref(false)
 
 const isActive = (path: string) => {
   if (path === '/') {
@@ -44,26 +53,40 @@ const isActive = (path: string) => {
       <nav class="fixed bottom-0 left-0 right-0 bg-background z-50 bg-elevated/50 backdrop-blur">
         <div class="max-w-2xl mx-auto px-4">
           <div class="flex justify-around items-center h-16">
-            <NuxtLink
+            <template  
               v-for="item in navItems"
               :key="item.to"
-              :to="item.to"
-              class="flex flex-col items-center justify-center gap-1 flex-1 py-2 transition-colors"
-              :class="[
-                isActive(item.to)
-                  ? 'text-primary'
-                  : 'text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300'
-              ]"
             >
-              <UIcon
-                :name="item.icon"
-                class="w-6 h-6"
+              <NuxtLink
+                v-if="item.to"
+                :to="item.to"
+                class="flex flex-col items-center justify-center gap-1 flex-1 py-2 transition-colors"
+                :class="[
+                  isActive(item.to)
+                    ? 'text-primary'
+                    : 'text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300'
+                ]"
+              >
+                <UIcon
+                  :name="item.icon"
+                  class="w-6 h-6"
+                />
+                <span class="text-xs font-medium">{{ item.label }}</span>
+              </NuxtLink>
+              <UButton
+                v-else
+                :icon="item.icon"
+                variant="solid"
+                size="xl"
+                class="rounded-full transform -translate-y-4 scale-130"
+                @click="item.onClick"
               />
-              <span class="text-xs font-medium">{{ item.label }}</span>
-            </NuxtLink>
+            </template>
           </div>
         </div>
       </nav>
     </div>
+    <UDrawer v-model:open="open" title="Add Transaction" description="Add a new transaction">
+    </UDrawer>
   </UDashboardGroup>
 </template>
